8d8954175b8707b5ded37bbf81c222a09450bf23
axlf0l333 <leolax1980@gmail.com>
2025-10-26 20:18:19 -0700

    Enhance process state reporting in cps function to include ZOMBIE and EMBRYO states

--- proc.c
+++ proc.c
@@ -561,6 +561,12 @@ cps()
       cprintf("%s \t %d \t RUNNING \t %d \n", p->name, p->pid, p->priority);
     else if(p->state == RUNNABLE)
       cprintf("%s \t %d \t RUNNABLE \t %d \n", p->name, p->pid, p->priority);
+    else if(p->state == ZOMBIE)
+      cprintf("%s \t %d \t ZOMBIE \t %d \n", p->name, p->pid, p->priority);
+    else if (p->state == EMBRYO)
+      cprintf("%s \t %d \t EMBRYO \t %d \n", p->name, p->pid, p->priority);
+    else
+      cprintf("%s \t %d \t UNUSED \t %d \n", p->name, p->pid, p->priority);
 }
   release(&ptable.lock);
   return 22;
