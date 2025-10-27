b4fb106c25cf35c5e7d69b5d8e27ada52d1ace17
axlf0l333 <leolax1980@gmail.com>
2025-10-26 19:35:50 -0700

    Fix priority range validation in nice command to enforce limits (0-20)

--- nice.c
+++ nice.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     }
     pid = atoi(argv[1]);
     priority = atoi(argv[2]);
-    if (priority < 0 || priority > 100)
+    if (priority < 0 || priority > 20)
     {
         printf(2, "Invalid Priority (0-20)!\n");
         exit();
